8ce189b5959d3599e821d3249caad5ec1214a663
Phil-0 <philipjfritz@gmail.com>
2021-02-03 03:23:09 -0800

    Updated version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='phil0-tornapi',
-    version='0.2.2',
+    version='0.3',
     description='An API wrapper for Torn City.',
     url='https://github.com/Phil-0/TornAPI/',
     license='GPLv3',
