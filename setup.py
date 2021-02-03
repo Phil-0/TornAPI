6f7282c10173da97e45c217d3f0876ea985ad2c4
Phil-0 <philipjfritz@gmail.com>
2021-02-03 03:01:55 -0800

    Changed version, added Torn username and ID to author.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='phil0-tornapi',
-    version='0.1',
+    version='0.2',
     description='An API wrapper for Torn City.',
     url='https://github.com/Phil-0/TornAPI/',
     license='GPLv3',
-    author='Phil-0',
+    author='Phil-0, PhilMe [2590086]',
     packages=find_packages(),
     install_requires=['requests~=2.5.4.1'],
     classifiers=[
